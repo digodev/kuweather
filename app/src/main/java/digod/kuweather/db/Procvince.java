7fe54c91ad4b51c1b4e3771d57f25ddb9443dd6f
digod <2798732983@qq.com>
2018-10-14 18:09:27 +0800

    加入库和表

---
+++ app/src/main/java/digod/kuweather/db/Procvince.java
@@ -0,0 +1,39 @@
+package digod.kuweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by digod on 2018/10/14.
+ */
+
+public class Procvince extends DataSupport {
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
